99986d59281e0f7cc8ac7563220e5174b8dc859f
Ajay <ajaysinghh2001@gmail.com>
2024-02-20 15:12:14 +0530

    edited for deployement

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const customerRoutes=require('./routes/customers');
-const productRoutes=require('./routes/products');
-const orderRoutes=require('./routes/orders');
-const authRoutes=require('./routes/auth');
+const customerRoutes = require('./routes/customers');
+const productRoutes = require('./routes/products');
+const orderRoutes = require('./routes/orders');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.use('/api/v1/auth',authRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/customers', customerRoutes);
+app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/orders', orderRoutes);
 
-app.use('/api/v1/customers',customerRoutes);
-
-app.use('/api/v1/products',productRoutes);
-
-app.use('/api/v1/orders',orderRoutes);
-
-
-app.use((req,res)=>{
-    res.status(404).send("Page Not Found")
+app.use((req, res, next) => {
+    res.status(404).send("Page Not Found");
 });
 
-app.listen(3000);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
 
